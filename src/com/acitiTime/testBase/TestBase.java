2363d1b815c413aca6742899c6d381bdffa2a2b5
AutomationforEasy <automationforeasy@gmail.com>
2017-01-27 19:33:48 -0800

    all files added

--- src/com/acitiTime/testBase/TestBase.java
+++ src/com/acitiTime/testBase/TestBase.java
@@ -4,21 +4,29 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 import java.util.Properties;
 
-public class TestBase {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.xml.sax.Locator;
+
+import com.acitiTime.utils.Utills;
+
+public class TestBase extends Utills{
 
 	public Properties Repository = new Properties();
 	
 	public File f;
 	public FileInputStream FI;
 	
+	
 	public void init() throws IOException{
-		
 		loadPropertiesFile();
+		selectBrowser(Repository.getProperty("browser"));
 		
 	}
-	public void loadPropertiesFile() throws IOException{
+	public void loadPropertiesFile(){
 		f = new File(System.getProperty("user.dir")+"\\src\\com\\acitiTime\\config\\config.properties");
 		FI = new FileInputStream(f);
 		Repository.load(FI);
@@ -37,24 +45,77 @@ public class TestBase {
 		
 		f = new File(System.getProperty("user.dir")+"\\src\\com\\acitiTime\\pagelocators\\timetracks.properties");
 		FI = new FileInputStream(f);
-		Repository.load(FI);
+		Repository.load(FI);	
+	}
+
+	public static WebElement getLocator(String locator) throws Exception {
+        String[] split = locator.split(":");
+		String locatorType = split[0];
+		String locatorValue = split[1];
+
+		if (locatorType.toLowerCase().equals("id"))
+			return driver.findElement(By.id(locatorValue));
+		else if (locatorType.toLowerCase().equals("name"))
+			return driver.findElement(By.name(locatorValue));
+		else if ((locatorType.toLowerCase().equals("classname"))
+				|| (locatorType.toLowerCase().equals("class")))
+			return driver.findElement(By.className(locatorValue));
+		else if ((locatorType.toLowerCase().equals("tagname"))
+				|| (locatorType.toLowerCase().equals("tag")))
+			return driver.findElement(By.className(locatorValue));
+		else if ((locatorType.toLowerCase().equals("linktext"))
+				|| (locatorType.toLowerCase().equals("link")))
+			return driver.findElement(By.linkText(locatorValue));
+		else if (locatorType.toLowerCase().equals("partiallinktext"))
+			return driver.findElement(By.partialLinkText(locatorValue));
+		else if ((locatorType.toLowerCase().equals("cssselector"))
+				|| (locatorType.toLowerCase().equals("css")))
+			return driver.findElement(By.cssSelector(locatorValue));
+		else if (locatorType.toLowerCase().equals("xpath"))
+			return driver.findElement(By.xpath(locatorValue));
+		else
+			throw new Exception("Unknown locator type '" + locatorType + "'");
 	}
 	
+	public static List<WebElement> getLocators(String locator) throws Exception {
+        String[] split = locator.split(":");
+		String locatorType = split[0];
+		String locatorValue = split[1];
+
+		if (locatorType.toLowerCase().equals("id"))
+			return driver.findElements(By.id(locatorValue));
+		else if (locatorType.toLowerCase().equals("name"))
+			return driver.findElements(By.name(locatorValue));
+		else if ((locatorType.toLowerCase().equals("classname"))
+				|| (locatorType.toLowerCase().equals("class")))
+			return driver.findElements(By.className(locatorValue));
+		else if ((locatorType.toLowerCase().equals("tagname"))
+				|| (locatorType.toLowerCase().equals("tag")))
+			return driver.findElements(By.className(locatorValue));
+		else if ((locatorType.toLowerCase().equals("linktext"))
+				|| (locatorType.toLowerCase().equals("link")))
+			return driver.findElements(By.linkText(locatorValue));
+		else if (locatorType.toLowerCase().equals("partiallinktext"))
+			return driver.findElements(By.partialLinkText(locatorValue));
+		else if ((locatorType.toLowerCase().equals("cssselector"))
+				|| (locatorType.toLowerCase().equals("css")))
+			return driver.findElements(By.cssSelector(locatorValue));
+		else if (locatorType.toLowerCase().equals("xpath"))
+			return driver.findElements(By.xpath(locatorValue));
+		else
+			throw new Exception("Unknown locator type '" + locatorType + "'");
+	}
 	
+	public WebElement getWebElement(String locator) throws Exception{
+		return getLocator(Repository.getProperty(locator));
+	}
 	
+	public List<WebElement> getWebElements(String locator) throws Exception{
+		return getLocators(Repository.getProperty(locator));
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public void closeBrowser(){
+		driver.quit();
+	}
 	
 }
